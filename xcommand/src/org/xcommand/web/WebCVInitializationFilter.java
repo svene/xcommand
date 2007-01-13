040c6d18119e898365486d5da01bcc2fda71c37b
svene <svene@be8a676c-8d15-0410-8a8d-dfc7c1491caa>
2007-01-13 12:38:11 +0000

    new XC filters
    git-svn-id: https://xcommand.svn.sourceforge.net/svnroot/xcommand/trunk@41 be8a676c-8d15-0410-8a8d-dfc7c1491caa

---
+++ xcommand/src/org/xcommand/web/WebCVInitializationFilter.java
@@ -0,0 +1,39 @@
+package org.xcommand.web;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Filter responsible for initializing the XC Context with webapp-related information (request, response, servletContext)
+ */
+public class WebCVInitializationFilter implements Filter
+{
+	public void init(FilterConfig filterConfig) throws ServletException
+	{
+		servletContext = filterConfig.getServletContext();
+	}
+
+	public void doFilter(ServletRequest aRequest, ServletResponse aResponse, FilterChain aFilterChain)
+		throws IOException, ServletException
+	{
+		// Retrieve XC Context:
+		Map ctx = XCRequestAttributeCV.getXcContext((HttpServletRequest) aRequest);
+
+		// Populate XC Context with webapp-related information (request, response, servletContext):
+		WebContextView.setRequest(ctx, (HttpServletRequest) aRequest);
+		WebContextView.setResponse(ctx, (HttpServletResponse) aResponse);
+		WebContextView.setServletContext(ctx, servletContext);
+		System.out.println("XCServletFilter.doFilter()");
+		aFilterChain.doFilter(aRequest, aResponse);
+	}
+
+	public void destroy()
+	{
+
+	}
+
+	ServletContext servletContext;
+}
--- xcommand/src/org/xcommand/web/XCRequestAttributeCV.java
+++ xcommand/src/org/xcommand/web/XCRequestAttributeCV.java
@@ -5,16 +5,23 @@ import java.util.Map;
 
 public class XCRequestAttributeCV
 {
-	public static final String KEY_XC_CONTEXT = "xc-context";
+
+// --- Access ---
 
 	public static Map getXcContext(HttpServletRequest aRequest)
 	{
 		return (Map) aRequest.getAttribute(KEY_XC_CONTEXT);
 	}
 
+// --- Setting ---
+
 	public static void setXcContext(HttpServletRequest aRequest, Map aCtx)
 	{
 		aRequest.setAttribute(KEY_XC_CONTEXT, aCtx);
 	}
 
+// --- Constants ---
+
+	public static final String NS = "org.xcommand.web.XCRequestAttributeCV.";
+	public static final String KEY_XC_CONTEXT = NS + "XC_CONTEXT";
 }
