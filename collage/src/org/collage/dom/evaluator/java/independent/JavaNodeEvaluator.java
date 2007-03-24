4fee50c781848bd96d7c13e915215b680ee01d98
svene <svene@be8a676c-8d15-0410-8a8d-dfc7c1491caa>
2007-03-24 05:19:30 +0000

    refactored javassist dir into java dir to prepare janino integration
    git-svn-id: https://xcommand.svn.sourceforge.net/svnroot/xcommand/trunk@46 be8a676c-8d15-0410-8a8d-dfc7c1491caa

---
+++ collage/src/org/collage/dom/evaluator/java/independent/JavaNodeEvaluator.java
@@ -0,0 +1,25 @@
+package org.collage.dom.evaluator.java.independent;
+
+import org.xcommand.core.IXCommand;
+import org.collage.dom.ast.JavaNode;
+import org.collage.dom.evaluator.EvaluationContextView;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Map;
+
+public class JavaNodeEvaluator implements IXCommand
+{
+	public void execute(Map aCtx)
+	{
+		JavaNode node = (JavaNode) EvaluationContextView.getNode(aCtx);
+		StringBuffer methodBody = (StringBuffer) aCtx.get("methodbody");
+//		System.out.println("*** javacode: '" + node.getValue() + "'");
+		String ss = "\t" + node.getValue() + "\n";
+		methodBody.append(ss);
+	}
+
+	private String decodedString(String aString)
+	{
+		return StringUtils.replace(aString, "#", "\"");
+	}
+}
---
+++ collage/src/org/collage/dom/evaluator/java/independent/VariableNodeEvaluator.java
@@ -0,0 +1,46 @@
+package org.collage.dom.evaluator.java.independent;
+
+import org.xcommand.core.IXCommand;
+import org.collage.dom.ast.VariableNode;
+import org.collage.dom.evaluator.EvaluationContextView;
+import org.collage.dom.evaluator.text.TextTemplate;
+import org.collage.dom.creationhandler.DomNodeCreationHandlerContextView;
+import org.collage.template.Template;
+import org.collage.template.TemplateSource;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.*;
+
+public class VariableNodeEvaluator implements IXCommand
+{
+
+	public VariableNodeEvaluator()
+	{
+		try
+		{
+			InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("javassist_var.txt");
+			Map ctx = new HashMap();
+			DomNodeCreationHandlerContextView.setProduceJavaSource(ctx, Boolean.FALSE);
+			template = new TextTemplate(new TemplateSource(is, ctx));
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void execute(Map aCtx)
+	{
+		Map ctx = new HashMap();
+		VariableNode node = (VariableNode) EvaluationContextView.getNode(aCtx);
+		String vn = node.getVariableName();
+		ctx.put("varName", vn);
+		String ss = template.getStringResult(ctx);
+
+		StringBuffer methodBody = (StringBuffer) aCtx.get("methodbody");
+		methodBody.append(ss);
+	}
+
+	private Template template;
+}
