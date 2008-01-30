239c1d0e90e573c7d3ee660cf78cb111dea13f93
svene <svene@be8a676c-8d15-0410-8a8d-dfc7c1491caa>
2008-01-30 18:41:39 +0000

    cleanup
    git-svn-id: https://xcommand.svn.sourceforge.net/svnroot/xcommand/trunk@91 be8a676c-8d15-0410-8a8d-dfc7c1491caa

--- xcommand/src/org/xcommand/misc/statemachine/DefaultStateTransitionBinder.java
+++ xcommand/src/org/xcommand/misc/statemachine/DefaultStateTransitionBinder.java
@@ -1,7 +1,6 @@
 package org.xcommand.misc.statemachine;
 
 import org.xcommand.core.IXCommand;
-import org.xcommand.pattern.observer.ConditionObserver;
 
 import java.util.Map;
 
@@ -10,25 +9,6 @@ public class DefaultStateTransitionBinder implements IStateTransitionBinder
 
 	public void bind(IState aFromState, Transition aTransition, final IState aToState)
 	{
-		aTransition.getPreExecuteNotifier().registerObserver(aFromState.getExitStateNotifier());
-		aTransition.getPostExecuteNotifier().registerObserver(aToState.getEnterStateNotifier());
-		aTransition.getPostExecuteNotifier().registerObserver(aToState.getExecuteStateNotifier());
-
-		aTransition.getPostExecuteNotifier().registerObserver(new IXCommand()
-		{
-			public void execute(Map aCtx)
-			{
-				StateCV.setState(aCtx, aToState);
-			}
-		});
-
-		aTransition.getPostExecuteNotifier().registerObserver(aFromState.getExecuteNotifier().getStopCommand());
-
-	}
-	public void bindOrg(IState aFromState, Transition aTransition, final IState aToState)
-	{
-		aFromState.getExecuteNotifier().registerObserver(aTransition);
-
 		aTransition.getPreExecuteNotifier().registerObserver(aFromState.getExitStateNotifier());
 		aTransition.getPostExecuteNotifier().registerObserver(aToState.getEnterStateNotifier());
 		aTransition.getPostExecuteNotifier().registerObserver(aToState.getExecuteStateNotifier());
