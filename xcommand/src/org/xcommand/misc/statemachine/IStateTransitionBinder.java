89286b8439f15292f37c52d10996c25c31f85ad0
svene <svene@be8a676c-8d15-0410-8a8d-dfc7c1491caa>
2008-01-30 18:41:56 +0000

    cleanup
    git-svn-id: https://xcommand.svn.sourceforge.net/svnroot/xcommand/trunk@92 be8a676c-8d15-0410-8a8d-dfc7c1491caa

--- xcommand/src/org/xcommand/misc/statemachine/IStateTransitionBinder.java
+++ xcommand/src/org/xcommand/misc/statemachine/IStateTransitionBinder.java
@@ -3,5 +3,4 @@ package org.xcommand.misc.statemachine;
 public interface IStateTransitionBinder
 {
 	void bind(IState aFromState, Transition aTransition, final IState aToState);
-	void bindOrg(IState aFromState, Transition aTransition, IState aToState);
 }
