fa20514d9a809ca365be9d97248f7fdabed506fb
svene <svene@be8a676c-8d15-0410-8a8d-dfc7c1491caa>
2008-02-04 18:33:28 +0000

    janino integration
    git-svn-id: https://xcommand.svn.sourceforge.net/svnroot/xcommand/trunk@115 be8a676c-8d15-0410-8a8d-dfc7c1491caa

---
+++ xcommand/src/org/xcommand/technology/janino/JaninoObjectCreator.java
@@ -0,0 +1,56 @@
+package org.xcommand.technology.janino;
+
+import org.codehaus.janino.JavaSourceClassLoader;
+import org.codehaus.janino.DebuggingInformation;
+import org.xcommand.core.IXCommand;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class JaninoObjectCreator
+{
+
+// --- Initialization ---
+
+	public JaninoObjectCreator(Map aClassMap)
+	{
+		classMap = aClassMap;
+		mrf = new MapResourceFinder(classMap);
+	}
+
+	public Class getClass(String aClassname)
+	{
+		ClassLoader parentClassLoader = getClass().getClassLoader();
+		String encoding = null;
+		ClassLoader cl = new JavaSourceClassLoader(parentClassLoader, mrf, encoding, DebuggingInformation.ALL);
+	 	String dotClassName = aClassname.replace('/', '.');
+		Class clazz = null;
+		try
+		{
+			clazz = cl.loadClass(dotClassName);
+		}
+		catch (ClassNotFoundException e)
+		{
+			throw new RuntimeException(e);
+		}
+		return clazz;
+
+	}
+	public Object getObject(String aClassname)
+	{
+		Class clazz = getClass(aClassname);
+		try
+		{
+			return clazz.newInstance();
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+// --- Implementation ---
+
+	Map classMap = new HashMap();
+	MapResourceFinder mrf;
+}
---
+++ xcommand/src/org/xcommand/technology/janino/MapResourceFinder.java
@@ -0,0 +1,55 @@
+package org.xcommand.technology.janino;
+
+import org.codehaus.janino.util.resource.Resource;
+import org.codehaus.janino.util.resource.ResourceFinder;
+
+import java.util.Map;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+
+/**
+ * Janino-ResourceFinder implementation providing the base for in-memory Java compilation.
+ * See {@ link http://www.janino.net/ } for details.
+ */
+public class MapResourceFinder extends ResourceFinder
+{
+	private final Map map;
+	private long lastModified = 0L;
+
+	public MapResourceFinder(Map map)
+	{
+		this.map = map;
+	}
+
+	public void setLastModified(long lastModified)
+	{
+		this.lastModified = lastModified;
+	}
+
+	public final Resource findResource(final String resourceName)
+	{
+		int p = resourceName.indexOf(".java");
+		final String s = resourceName.substring(0, p);
+		final byte[] ba = (byte[]) this.map.get(s);
+		if (ba == null) return null;
+
+		return new Resource()
+		{
+			public InputStream open() throws IOException
+			{
+				return new ByteArrayInputStream(ba);
+			}
+
+			public String getFileName()
+			{
+				return s;
+			}
+
+			public long lastModified()
+			{
+				return MapResourceFinder.this.lastModified;
+			}
+		};
+	}
+}
