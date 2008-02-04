fe06cca0555a9dd821cb9d96dd1d5838767a55ac
svene <svene@be8a676c-8d15-0410-8a8d-dfc7c1491caa>
2008-02-04 18:32:37 +0000

    jetty integration
    git-svn-id: https://xcommand.svn.sourceforge.net/svnroot/xcommand/trunk@114 be8a676c-8d15-0410-8a8d-dfc7c1491caa

---
+++ xcommand/src/org/xcommand/integration/jetty/JettyCV.java
@@ -0,0 +1,38 @@
+package org.xcommand.integration.jetty;
+
+import org.mortbay.jetty.Request;
+import org.mortbay.jetty.Response;
+
+import java.util.Map;
+
+public class JettyCV
+{
+
+// --- Access ---
+
+	public static Request getJettyRequest(Map aCtx)
+	{
+		return (Request) aCtx.get(KEY_JETTY_REQUEST);
+	}
+	public static Response getJettyResponse(Map aCtx)
+	{
+		return (Response) aCtx.get(KEY_JETTY_RESPONSE);
+	}
+
+// --- Setting ---
+
+	public static void setJettyRequest(Map aCtx, Request aJettyRequest)
+	{
+		aCtx.put(KEY_JETTY_REQUEST, aJettyRequest);
+	}
+	public static void setJettyResponse(Map aCtx, Response aJettyResponse)
+	{
+		aCtx.put(KEY_JETTY_RESPONSE, aJettyResponse);
+	}
+
+// --- Implementation ---
+
+	private static final String NS = "org.xcommand.integration.jetty.JettyCV.";
+	private static final String KEY_JETTY_REQUEST = NS + "JETTY_REQUEST";
+	private static final String KEY_JETTY_RESPONSE = NS + "JETTY_RESPONSE";
+}
---
+++ xcommand/src/org/xcommand/integration/jetty/StaticXCJettyHandler.java
@@ -0,0 +1,67 @@
+package org.xcommand.integration.jetty;
+
+import org.mortbay.jetty.handler.AbstractHandler;
+import org.mortbay.jetty.Request;
+import org.mortbay.jetty.HttpConnection;
+import org.mortbay.jetty.Response;
+import org.xcommand.web.WebContextView;
+import org.xcommand.core.IXCommand;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+public class StaticXCJettyHandler extends AbstractHandler
+{
+	public void handle(String aString, HttpServletRequest aHttpServletRequest,
+		HttpServletResponse aHttpServletResponse, int i) throws IOException, javax.servlet.ServletException
+	{
+		Request base_request = (aHttpServletRequest instanceof Request) ?
+			(Request) aHttpServletRequest : HttpConnection.getCurrentConnection().getRequest();
+		Response base_response = (aHttpServletResponse instanceof Response) ?
+			(Response)aHttpServletResponse : HttpConnection.getCurrentConnection().getResponse();
+		base_request.setHandled(true);
+
+		Map ctx = new HashMap();
+		JettyCV.setJettyRequest(ctx, base_request);
+		JettyCV.setJettyResponse(ctx, base_response);
+		WebContextView.setRequest(ctx, aHttpServletRequest);
+		WebContextView.setResponse(ctx, aHttpServletResponse);
+
+		cmd.execute(ctx);
+	}
+
+	static
+	{
+		// Determine default commandlet:
+		String classname;
+		classname = System.getProperty("xc.dcc");
+		if (classname == null)
+		{
+			classname = System.getProperty("xc.default-command-classname");
+		}
+		if (classname == null)
+		{
+			classname = "";
+			System.err.println("XC-default-commandlet-classname neither defined by System-property " + "" +
+				"'xc.default-command-classname' nor 'xc.dcc'. ");
+			throw new RuntimeException("XC-default-commandlet-classname not defined");
+		}
+		try
+		{
+			Class clazz = Class.forName(classname);
+			if (IXCommand.class.isAssignableFrom(clazz))
+			{
+				cmd = (IXCommand) clazz.newInstance();
+			}
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static IXCommand cmd;
+}
