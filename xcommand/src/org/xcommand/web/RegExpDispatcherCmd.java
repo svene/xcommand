a0757145fe11502b8b77b276ff72a41132efe475
svene <svene@be8a676c-8d15-0410-8a8d-dfc7c1491caa>
2008-02-04 18:37:11 +0000

    dispatcher
    git-svn-id: https://xcommand.svn.sourceforge.net/svnroot/xcommand/trunk@119 be8a676c-8d15-0410-8a8d-dfc7c1491caa

---
+++ xcommand/src/org/xcommand/web/RegExpDispatcherCmd.java
@@ -0,0 +1,50 @@
+package org.xcommand.web;
+
+import org.xcommand.core.IXCommand;
+import org.xcommand.web.WebContextView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class RegExpDispatcherCmd implements IXCommand
+{
+
+	public void setCommands(Map aCommands)
+	{
+		commands = aCommands;
+	}
+
+	public void execute(Map aCtx)
+	{
+		HttpServletRequest request = WebContextView.getRequest(aCtx);
+//		String path = request.getPathInfo();
+		String path = request.getRequestURI();
+		Iterator it = commands.keySet().iterator();
+		boolean found = false;
+		IXCommand cmd = null;
+		while (!found && it.hasNext())
+		{
+			String ps = (String) it.next();
+			Pattern p = Pattern.compile(ps);
+			Matcher m = p.matcher(path);
+			if (m.matches())
+			{
+				cmd = (IXCommand) commands.get(ps);
+				found = true;
+			}
+		}
+
+		if (cmd != null)
+		{
+			cmd.execute(aCtx);
+		}
+
+	}
+
+	// Commands identified by pattern (e.g. '^/bla.*') used as key for this map:
+	private Map commands;
+
+}
