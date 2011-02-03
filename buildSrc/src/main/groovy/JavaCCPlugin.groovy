1a3eae0cd4cc0300e8074711b6124b9904002a60
Sven Ehrke <sven.ehrke@canoo.com>
2011-02-03 20:45:06 +0100

    further work on JavaCC plugin

--- buildSrc/src/main/groovy/JavaCCPlugin.groovy
+++ buildSrc/src/main/groovy/JavaCCPlugin.groovy
@@ -5,6 +5,8 @@ import org.gradle.api.tasks.Copy
 class JavaCCPlugin implements Plugin<Project>  {
 	def void apply(Project project) {
 
+		project.convention.plugins.javaccg = new JavaCCPluginConvention()
+
 		project.task ('copyToLib', dependsOn: project.configurations.compile, type: Copy) {
 	        into('build/_artifacts')
     	    from project.configurations.default
@@ -19,26 +21,41 @@ class JavaCCPlugin implements Plugin<Project>  {
 			rename('javacc-4.0.jar', 'javacc.jar')
 		}
 
-		project.task('jcc', dependsOn: 'renameJCC') {
-			def inputFile = 'templateparser.jj' 
-			def genDir = 'build/org/collage/jcc'
+		project.task('javacc', dependsOn: 'renameJCC') {
 
-			inputs.file inputFile
-			outputs.dir genDir
+//			inputs.file project.convention.plugins.javaccg.inputFile
+//			outputs.dir project.convention.plugins.javaccg.genDir
 
 			doLast {
-				println "generate jcc source"
+				println "generate javacc source to: $project.convention.plugins.javaccg.genDir"
 				ant.mkdir(dir: 'build/_artifacts')
 				ant.mkdir(dir: 'build/org/collage/jcc')
-				ant.javacc(target: inputFile, outputdirectory: genDir, javacchome: 'build/_artifacts')
+				ant.javacc(target: project.convention.plugins.javaccg.inputFile, outputdirectory: project.convention.plugins.javaccg.genDir, javacchome: 'build/_artifacts')
+				project.sourceSets.main.java.srcDir(project.convention.plugins.javaccg.genDir)
 			}
-
-			project.sourceSets.main.java.srcDir(genDir)
+			// println "***: genDir= ${project.convention.plugins.javaccg.genDir}" // --> default-gendir
 		}
 
-		project.compileJava.dependsOn(project.jcc)
+		project.compileJava.dependsOn(project.javacc)
 
 	}
+
+	class JavaCCPluginConvention {
+//		def inputFile = 'xxx.jj'
+//		def genDir = 'default-gendir'
+
+		def javaccg(Closure closure) {
+			closure.delegate = this
+			closure()
+		}
+
+		def doit() {
+			println "in doit"
+		}
+
+	}
+
+
 }
 
 
