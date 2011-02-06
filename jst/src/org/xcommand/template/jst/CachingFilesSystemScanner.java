3d9afdc07b0d0acd221abcc64af136c6ef80789e
svene <svene@be8a676c-8d15-0410-8a8d-dfc7c1491caa>
2011-02-06 09:16:09 +0000

    added missing file CachingFilesSystemScanner.java
    git-svn-id: https://xcommand.svn.sourceforge.net/svnroot/xcommand/trunk@148 be8a676c-8d15-0410-8a8d-dfc7c1491caa

---
+++ jst/src/org/xcommand/template/jst/CachingFilesSystemScanner.java
@@ -0,0 +1,81 @@
+package org.xcommand.template.jst;
+
+import org.xcommand.core.ICommand;
+import org.xcommand.core.DynaBeanProvider;
+import org.xcommand.core.IDynaBeanProvider;
+import org.xcommand.core.ClassAndMethodKeyProvider;
+import org.xcommand.pattern.observer.BasicNotifier;
+import org.xcommand.pattern.observer.INotifier;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CachingFilesSystemScanner implements ICommand
+{
+	public void execute()
+	{
+		List rootDirs = fileSystemScannerCV.getRootDirs();
+
+		FileSystemScanner fssc = new FileSystemScanner();
+		fssc.setRootDirs(rootDirs);
+		fssc.getFileFoundNotifier().registerObserver(new FileFoundHandler());
+		HashMap changedFiles = new HashMap();
+		cachingFilesSystemScannerCV.setChangedFiles(changedFiles);
+		if (cachingFilesSystemScannerCV.getCurrentFiles() == null)
+		{
+			cachingFilesSystemScannerCV.setCurrentFiles(new HashMap());
+		}
+		fssc.execute();
+		if (changedFiles.size() > 0)
+		{
+			changedFilesNotifier.execute();
+		}
+	}
+
+	public INotifier getChangedFilesNotifier()
+	{
+		return changedFilesNotifier;
+	}
+
+// --- Implementation ---
+
+	private class FileFoundHandler implements ICommand
+	{
+
+		public void execute()
+		{
+			File file = fileSystemScannerCV.getFile();
+			String key = file.getAbsolutePath();
+			Map currentFiles = cachingFilesSystemScannerCV.getCurrentFiles();
+			Map changedFiles = cachingFilesSystemScannerCV.getChangedFiles();
+			if (!currentFiles.containsKey(key))
+			{
+				System.out.println("new file found: " + key);
+				FileMapEntry fme = new FileMapEntry();
+				fme.file = file;
+				fme.key = key;
+				fme.lastmodified = file.lastModified();
+				fme.rootDir = fileSystemScannerCV.getRootDir();
+				currentFiles.put(key, fme);
+				changedFiles.put(key, fme);
+			}
+			else
+			{
+				FileMapEntry fme = (FileMapEntry) currentFiles.get(key);
+				if (fme.file.lastModified() > fme.lastmodified)
+				{//reload file
+					changedFiles.put(key, fme);
+				}
+			}
+		}
+	}
+
+	private INotifier changedFilesNotifier = new BasicNotifier();
+	private IDynaBeanProvider dbp = DynaBeanProvider.newThreadBasedDynabeanProvider(new ClassAndMethodKeyProvider());
+	IFileSystemScannerCV fileSystemScannerCV = (IFileSystemScannerCV) dbp.newBeanForInterface(IFileSystemScannerCV.class);
+	ICachingFilesSystemScannerCV cachingFilesSystemScannerCV = (ICachingFilesSystemScannerCV) dbp.newBeanForInterface(
+		ICachingFilesSystemScannerCV.class);
+
+}
