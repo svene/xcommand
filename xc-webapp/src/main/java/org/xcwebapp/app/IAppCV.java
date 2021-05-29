d36895431ee12a5cae53ff7cbd8103560afab215
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2021-05-29 09:02:06 +0200

    - removed xc-webapp: -- too much trouble getting gretty to work -- with modern frontend frameworks like Angular etc. supporting this is not important anymore - './gradlew clean build' successful

--- xc-webapp/src/main/java/org/xcwebapp/app/IAppCV.java
+++
@@ -1,7 +0,0 @@
-package org.xcwebapp.app;
-
-public interface IAppCV
-{
-	public AppBeanProvider getAppBeanProvider();
-	public void setAppBeanProvider(AppBeanProvider aAppBeanProvider);
-}
--- xc-webapp/src/main/java/org/xcwebapp/tmp/ISpringAppCtxCV.java
+++
@@ -1,9 +0,0 @@
-package org.xcwebapp.tmp;
-
-import org.springframework.context.ApplicationContext;
-
-public interface ISpringAppCtxCV
-{
-	public ApplicationContext getApplicationContext();
-	public void setApplicationContext(ApplicationContext aApplicationContext);
-}
