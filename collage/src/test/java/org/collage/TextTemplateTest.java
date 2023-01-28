06fa2c45a81e03cd7ee7d7b2d60e7e17356b3448
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-01-28 09:50:43 +0100

    improved test

--- collage/src/test/java/org/collage/TextTemplateTest.java
+++ collage/src/test/java/org/collage/TextTemplateTest.java
@@ -4,11 +4,11 @@ import org.collage.dom.creationhandler.IDomNodeCreationHandlerCV;
 import org.collage.dom.evaluator.common.IStringHandlerCV;
 import org.collage.template.JavassistTemplateCompiler;
 import org.collage.template.TemplateCV;
-import org.collage.template.TemplateCommand;
 import org.collage.template.TextTemplateCompiler;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 import org.xcommand.core.*;
 
 import java.io.StringWriter;
@@ -18,12 +18,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TextTemplateTest {
+	static String FIRSTNAME = "Denis";
+	static String LASTNAME = "Bogan";
+
 	StringWriter stringWriter;
 
 	@BeforeEach
 	public void initializeContext() {
 		TCP.pushContext(new HashMap<>());
-		TCP.getContext().put("firstname", "Uli");
+		TCP.getContext().put("firstname", FIRSTNAME);
 
 		stringWriter = new StringWriter();
 	}
@@ -33,62 +36,84 @@ public class TextTemplateTest {
 		TCP.popContext();
 	}
 
-	@Test
-	public void test1() {
-		new TextTemplateCompiler().newTemplateCommandFromString("hallo ${firstname}.\nWie gehts?\n").execute();
-		assertEquals("hallo Uli.\nWie gehts?\n", stringHandlerCV.getString());
-	}
-
-	@Test
-	public void test2() {
-		var tc = new TextTemplateCompiler().newTemplateCommandFromString("hallo ${firstname}.\nWie gehts?\n");
-		tc.setWriter(stringWriter);
-		tc.execute();
-		assertEquals("hallo Uli.\nWie gehts?\n", stringWriter.toString());
-	}
-
-	@Test
-	public void verfiyProperFunctionIfInputComesFromFile() {
-		var tc = new TextTemplateCompiler().newTemplateCommandFromResourceName("in.txt");
-		tc.setWriter(stringWriter);
-		tc.execute();
-		assertEquals("Hallo Uli! Willkommen bei uns.\n<?java int i = 1 ?>d\n", stringWriter.toString());
-	}
-
-	@Test
-	public void test5() {
-		domNodeCreationHandlerCV.setProduceJavaSource(Boolean.FALSE);
-		var tc = new TextTemplateCompiler().newTemplateCommandFromString("hallo ${firstname}.\nWie gehts?\n");
-
-		TCP.getContext().put("firstname", "Sven");
-		tc.setWriter(stringWriter);
-		tc.execute();
-		assertEquals("hallo Sven.\nWie gehts?\n", stringWriter.toString());
-	}
-
 	private String createTemplate(String in) {
 		new TextTemplateCompiler().newTemplateCommandFromString(in).execute();
 		return stringHandlerCV.getString();
 	}
 
+	@Nested
+	class Basic {
+		@Test
+		public void test1() {
+			String out = createTemplate("hallo ${firstname}.\nWie gehts?\n");
+			assertEquals("hallo %s.\nWie gehts?\n".formatted(FIRSTNAME), out);
+		}
+		@Test
+		public void test2() {
+			var tc = new TextTemplateCompiler().newTemplateCommandFromString("hallo ${firstname}.\nWie gehts?\n");
+			tc.setWriter(stringWriter);
+			tc.execute();
+			assertEquals("hallo %s.\nWie gehts?\n".formatted(FIRSTNAME), stringWriter.toString());
+		}
+
+		@Test
+		public void verfiyProperFunctionIfInputComesFromFile() {
+			var tc = new TextTemplateCompiler().newTemplateCommandFromResourceName("in.txt");
+			tc.setWriter(stringWriter);
+			tc.execute();
+			assertEquals("Hallo %s! Willkommen bei uns.\n<?java int i = 1 ?>d\n".formatted(FIRSTNAME), stringWriter.toString());
+		}
+
+		@Test
+		public void test5() {
+			domNodeCreationHandlerCV.setProduceJavaSource(Boolean.FALSE);
+			var tc = new TextTemplateCompiler().newTemplateCommandFromString("hallo ${firstname}.\nWie gehts?\n");
+
+			TCP.getContext().put("firstname", FIRSTNAME);
+			tc.setWriter(stringWriter);
+			tc.execute();
+			assertEquals("hallo %s.\nWie gehts?\n".formatted(FIRSTNAME), stringWriter.toString());
+		}
+
+	}
+
 	/**
-	 * Demonstrate recursive template resolution
+	 * Demonstrate recursive template handling
 	 */
-	@Test
-	public void testRecursiveInlining() {
-		TCP.getContext().put("name", "${firstname} ${lastname}");
-		assertThat(createTemplate("hallo ${name}. Wie gehts?"))
-			.isEqualTo("hallo ${firstname} ${lastname}. Wie gehts?");
+	@Nested
+	class Recursive {
+		@Test
+		public void recursive_placeholder_handling() {
+			TCP.getContext().put("name", "${firstname} ${lastname}");
+			assertThat(
+				createTemplate("hallo ${name}. Wie gehts?")
+			).isEqualTo("hallo ${firstname} ${lastname}. Wie gehts?");
+		}
 
-		assertThat(createTemplate("hallo ${firstname} ${lastname}. Wie gehts?")).isEqualTo("hallo Uli ${lastname}. Wie gehts?");
-		assertThat(createTemplate("hallo Uli ${lastname}. Wie gehts?")).isEqualTo("hallo Uli ${lastname}. Wie gehts?");
+	}
 
-		var cmd = new JavassistTemplateCompiler().newTemplateCommandFromString("hallo Uli ${lastname}. Wie gehts?");
-		TCP.getContext().put("firstname", "Uli");
-		TCP.getContext().put("lastname", "Ehrke");
-		TemplateCV.setWriter(stringWriter);
-		cmd.execute();
-		assertEquals("hallo Uli Ehrke. Wie gehts?", stringWriter.toString());
+	@Nested
+	class Misc {
+		@Test
+		public void unknowns_are_not_replaced() {
+			assertThat(createTemplate("hallo ${lastname}. Wie gehts?")).isEqualTo("hallo ${lastname}. Wie gehts?");
+		}
+		@Test
+		public void only_knowns_are_replaced() {
+			assertThat(
+				createTemplate("hallo ${firstname} ${lastname}. Wie gehts?")
+			).isEqualTo("hallo %s ${lastname}. Wie gehts?".formatted(FIRSTNAME));
+		}
+		@Test
+		public void javassist_template() {
+			var cmd = new JavassistTemplateCompiler().newTemplateCommandFromString("hallo ${lastname}. Wie gehts?");
+			TCP.getContext().put("firstname", FIRSTNAME);
+			TCP.getContext().put("lastname", LASTNAME);
+			TemplateCV.setWriter(stringWriter);
+			cmd.execute();
+			assertEquals("hallo %s. Wie gehts?".formatted(LASTNAME), stringWriter.toString());
+		}
+
 	}
 
 	private final IDynaBeanProvider dbp = DynaBeanProvider.newThreadClassMethodInstance();
